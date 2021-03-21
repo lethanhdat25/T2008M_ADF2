6e87fb6fcd100307e15f8512f3110236d3e3501f
lethanhdat25 <70951430+lethanhdat25@users.noreply.github.com>
2021-03-21 20:12:38 +0700

    up

--- src/sample/tapLam/Controller.java
+++ src/sample/tapLam/Controller.java
@@ -1,83 +1,113 @@
 package sample.tapLam;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
 import javafx.collections.ObservableList;
-import javafx.collections.ObservableSet;
-import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.text.Text;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.HashSet;
 import java.util.ResourceBundle;
 
 public class Controller implements Initializable {
     public TextField txtName;
     public TextField txtAge;
     public TextField txtMark;
-    public String txt="";
-    public TableView<SinhVien> txtView;
-    public Button button;
-    public TableColumn<SinhVien,String> columnName;
-    public TableColumn<SinhVien,Integer> columnAge;
-    public TableColumn<SinhVien,Integer> columnMark;
-    ObservableList<SinhVien> ds= FXCollections.observableArrayList();
-    boolean run=true;
+    public Button btnSort;
+    public Text txtValidate;
+    public TableView<SinhVien> txtRs;
+    public TableColumn<SinhVien,String> tenSV;
+    public TableColumn<SinhVien,Integer> tuoiSV;
+    public TableColumn<SinhVien,Integer> diemSV;
+
+    public static Integer identity = 0;
+    public static SinhVien editSinhVien;
+    ObservableList<SinhVien> ds = FXCollections.observableArrayList();
+    static boolean sortType = false;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        columnName.setCellValueFactory(new PropertyValueFactory<SinhVien,String>("name"));
-        columnAge.setCellValueFactory(new PropertyValueFactory<SinhVien,Integer>("age"));
-        columnMark.setCellValueFactory(new PropertyValueFactory<SinhVien,Integer>("mark"));
+        tenSV.setCellValueFactory(new PropertyValueFactory<SinhVien,String>("name"));
+        tuoiSV.setCellValueFactory(new PropertyValueFactory<SinhVien,Integer>("age"));
+        diemSV.setCellValueFactory(new PropertyValueFactory<SinhVien,Integer>("mark"));
     }
-    public void inra(){
+
+    public void input(){
         try {
-            String n=txtName.getText();
-            int a=Integer.parseInt(txtAge.getText());
-            int m=Integer.parseInt(txtMark.getText());
-            if (!n.isEmpty()){
-                SinhVien sv=new SinhVien(n,a,m);
-                ds.add(sv);
+            String n =  txtName.getText();
+            int a = Integer.parseInt(txtAge.getText());
+            int m = Integer.parseInt(txtMark.getText());
+            if(!n.isEmpty()){
+                if(editSinhVien != null){
+                    editSinhVien.setName(n);
+                    editSinhVien.setAge(a);
+                    editSinhVien.setMark(m);
+                    for(SinhVien s:ds){
+                        if(s.getId()== editSinhVien.getId()){
+                            s = editSinhVien;
+                            break;
+                        }
+                    }
+                    txtRs.refresh();
+                }else {
+                    identity++;
+                    SinhVien s = new SinhVien(identity,n,a,m);
+                    ds.add(s);
+                    txtRs.setItems(ds);
+                }
+                editSinhVien = null;
+                txtValidate.setText("");
                 txtName.setText("");
                 txtAge.setText("");
                 txtMark.setText("");
-                txtView.setItems(ds);
-            }else{
-                txtName.setText("Moi ban nhap ten");
+
+            }else {
+                txtValidate.setText("Vui lòng nhập tên, tuổi và điểm thi");
+                txtValidate.setDisable(false);
             }
         }catch (Exception e){
-            txtName.setPromptText("Vui lòng nhập tên: ");
-            txtAge.setPromptText("Vui lòng nhập tuoi: ");
-            txtMark.setPromptText("Vui lòng nhập diem: ");
+            txtValidate.setText("Vui lòng nhập tên, tuổi và điểm thi");
+            txtValidate.setDisable(false);
         }
+
     }
-    public void sapXep(){
-        if (run){
+
+    public void sort(){
+        sortType =  !sortType;
+        if(sortType){
             Collections.sort(ds, new Comparator<SinhVien>() {
                 @Override
                 public int compare(SinhVien o1, SinhVien o2) {
-                    return o2.getMark()-o1.getMark();
+                    return o1.getMark() - o2.getMark();
                 }
             });
-            button.setText("thap den cao");
-
-            run=false;
+            btnSort.setText("Thap den cao");
         }else{
             Collections.sort(ds, new Comparator<SinhVien>() {
                 @Override
                 public int compare(SinhVien o1, SinhVien o2) {
-                    return o1.getMark()-o2.getMark();
+                    return o2.getMark() - o1.getMark();
                 }
             });
-            button.setText("Cao den thap");
-            run=true;
+            btnSort.setText("Cao den thap");
         }
+        String txt = "";
+        for(SinhVien i:ds){
+            txt+= i.getName()+"--"+i.getAge()+"--"+i.getMark()+"\n";
+        }
+        //txtRs.setText(txt);
     }
 
-
+    public void update(){
+        SinhVien s = txtRs.getSelectionModel().getSelectedItem();
+        txtName.setText(s.getName());
+        txtAge.setText(s.getAge().toString());
+        txtMark.setText(s.getMark().toString());
+        editSinhVien = s;
+    }
 }
